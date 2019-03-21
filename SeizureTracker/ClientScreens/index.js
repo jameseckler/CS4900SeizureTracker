711b61730495f71ff3a1f498a0a93a80003d2850
James Eckler <jameseckler1994@yahoo.com>
2019-03-20 21:35:01 -0400

    Basic outline of client components

--- SeizureTracker/ClientScreens/index.js
+++ SeizureTracker/ClientScreens/index.js
@@ -6,33 +6,65 @@ import {createSwitchNavigator, createAppContainer} from 'react-navigation';
 
 class DashboardScreen extends Component{
 
-    displayUser = () => {
-    
-      var uid, email, name;
+  displayUser = () => {
   
-      user = firebase.auth().currentUser;
-  
-      if (user != null) {
-        email = user.email;
-        uid = user.uid;
-        name = user.displayName;
-      }
-  
-    };
-  
-    render() {
-      this.displayUser();
-      return(
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-          <Text>Welcome, {name}!</Text>
-          
-          <Button title="Pet List"/>
-          <Button title="Vet List"/>
-          <Button title="Communications"/>
-          <Button title="About Seizures"/>
-          <Button title="Create Log"/>
-          <Button title="View Logs"/>
-        </View>
-      );
+    var uid, email, name;
+
+    user = firebase.auth().currentUser;
+
+    if (user != null) {
+      email = user.email;
+      uid = user.uid;
+      name = user.displayName;
     }
+
+  };
+
+  render() {
+    this.displayUser();
+    return(
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+        <Text>Welcome, {name}!</Text>
+        
+        <Button title="Pet List"/>
+        <Button title="Vet List"/>
+        <Button title="Communications"/>
+        <Button title="About Seizures"/>
+        <Button title="Create Log"/>
+        <Button title="View Logs"/>
+      </View>
+    );
   }
+}
+
+class PetListScreen extends Component {
+  // Pet List that routes to pet info screen
+
+  // Add Pet Button
+}
+
+class AddPetScreen extends Component {
+  // Fields for all pet info
+
+  // Submit button
+}
+
+class PetInfoScreen extends Component {
+  // Display pet info
+
+  // List of logs for pet
+}
+
+class VetListScreen extends Component {
+  // Vet List that routes to vet info
+
+  // Add vet button
+}
+
+class CreateLogScreen extends Component {
+  
+}
+
+class PetLogScreen extends Component {
+
+}
