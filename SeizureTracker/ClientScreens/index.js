40e9cf6752f927965a87f2b26ad7e37946afbf57
James Eckler <jameseckler1994@yahoo.com>
2019-03-20 21:56:31 -0400

    Outline for vet screens

--- SeizureTracker/ClientScreens/index.js
+++ SeizureTracker/ClientScreens/index.js
@@ -61,10 +61,22 @@ class VetListScreen extends Component {
   // Add vet button
 }
 
+class VetInfoScreen extends Component {
+  // List vet information
+
+  // Unlink button
+}
+
 class CreateLogScreen extends Component {
-  
+  // Fields for log creation
+
+  // Submit button
 }
 
 class PetLogScreen extends Component {
+  // Display info for log
+}
 
+class CommunicationScreen extends Component {
+  // List of veterinarians and recent messages
 }
---
+++ SeizureTracker/VetScreens/index.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import firebase from 'firebase';
+import 'firebase/firestore';
+import {createSwitchNavigator, createAppContainer} from 'react-navigation';
+
+class VetHome extends Component {
+    // Buttons for vet home screen
+}
+
+class ExportScreen extends Component {
+    // Fields/buttons for exporting data
+}
+
+class DataScreen extends Component {
+    // Display data
+}
+
+class ClientListScreen extends Component {
+    // List of clients that link to client info
+}
+
+class ClientInfoScreen extends Component {
+    // Display client info
+
+    // Display client pets
+
+    // Unlink client button
+}
+
+class SearchScreen extends Component {
+    // Search input
+
+    // Display results
+}
