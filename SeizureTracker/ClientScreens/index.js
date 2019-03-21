bd0824ae6a8077819b351afbe54f6bfe1f90ff0f
Joshua <jt49441@gmail.com>
2019-03-21 18:41:19 -0400

    v0.2.5 - Added tab navigator

--- SeizureTracker/ClientScreens/index.js
+++ SeizureTracker/ClientScreens/index.js
@@ -1,14 +1,33 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 import 'firebase/firestore';
+import {createBottomTabNavigator} from 'react-navigation';
+import ClientHome from './screens/ClientHome';
+import ClientLogs from './screens/ClientLogs';
+import ClientSettings from './screens/ClientSettings';
+import Icon from 'react-native-vector-icons/Ionicons';
 
-export default class ClientHome extends Component{
-  
-    render() {
-      return(
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-          <Text>Client Home</Text>
-        </View>
-      );
-    }
-  }
+
+export default createBottomTabNavigator({
+  Home: { screen: ClientHome,
+  navigationOptions: {
+    tabBarLabel: 'Home',
+    tabBarIcon: ({tintColor}) => (
+      <Icon name="ios-home" size={24} />
+    )
+  }},
+  Logs: { screen: ClientLogs,
+  navigationOptions: {
+    tabBarLabel: 'Logs',
+    tabBarIcon: ({tintColor}) => (
+      <Icon name="ios-albums" size={24} />
+    )
+  }},
+  Settings: { screen: ClientSettings,
+  navigationOptions: {
+    tabBarLabel: 'Settings',
+    tabBarIcon: ({tintColor}) => (
+      <Icon name="ios-settings" size={24} />
+    )
+  }}
+});
