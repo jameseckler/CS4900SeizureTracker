20fe806c742a06662d68fe9e9b9e269068b0af8e
Joshua <jt49441@gmail.com>
2019-03-21 20:01:06 -0400

    v0.3.0 - Added basic navigation tab and stack navigation, still fleshing out

--- SeizureTracker/ClientScreens/index.js
+++ SeizureTracker/ClientScreens/index.js
@@ -3,31 +3,53 @@ import { StyleSheet, Text, View, Button } from 'react-native';
 import 'firebase/firestore';
 import {createBottomTabNavigator} from 'react-navigation';
 import ClientHome from './screens/ClientHome';
-import ClientLogs from './screens/ClientLogs';
+import ClientPets from './screens/ClientPets';
 import ClientSettings from './screens/ClientSettings';
 import Icon from 'react-native-vector-icons/Ionicons';
 
-
 export default createBottomTabNavigator({
   Home: { screen: ClientHome,
+  // individual tab options
   navigationOptions: {
     tabBarLabel: 'Home',
     tabBarIcon: ({tintColor}) => (
       <Icon name="ios-home" size={24} />
     )
   }},
-  Logs: { screen: ClientLogs,
+  Pets: { screen: ClientPets,
+  // individual tab options
   navigationOptions: {
-    tabBarLabel: 'Logs',
+    tabBarLabel: 'Pets',
     tabBarIcon: ({tintColor}) => (
       <Icon name="ios-albums" size={24} />
     )
   }},
   Settings: { screen: ClientSettings,
+  // individual tab options
   navigationOptions: {
     tabBarLabel: 'Settings',
     tabBarIcon: ({tintColor}) => (
       <Icon name="ios-settings" size={24} />
     )
   }}
+}, {//routerconfig
+  initialRouteName: 'Home',
+  order: ['Home', 'Pets', 'Settings'],
+  //navigation options for complete tab navigator
+  navigationOptions: {
+    tabBarVisible: true
+  },
+  tabBarOptions:{
+    style: {
+      //backgroundColor: 'blue',
+    }
+  } 
+}
+);
+
+const styles = StyleSheet.create({
+  background: {
+    width: '100%',
+    height: '100%',
+  }
 });
--- SeizureTracker/ClientScreens/screens/ClientHome/index.js
+++ SeizureTracker/ClientScreens/screens/ClientHome/index.js
@@ -1,14 +1,44 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, ImageBackground } from 'react-native';
 import 'firebase/firestore';
+import {createStackNavigator, createAppContainer} from 'react-navigation';
+import Pets from './screens/Pets';
+
+const background = require('./assets/background.png');
 
 export default class ClientHome extends Component{
   
     render() {
       return(
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-          <Text>Client Home</Text>
-        </View>
+        <ImageBackground
+          source={background}
+          style={styles.background}
+          resizeMode="stretch"
+
+        >
+          <AppContainer />
+        </ImageBackground>
       );
     }
   }
+
+const AppStackNavigator = createStackNavigator({
+  Login:{
+    screen: Pets
+  }
+});
+
+const AppContainer = createAppContainer(AppStackNavigator);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  background: {
+    width: '100%',
+    height: '100%',
+  },
+})
