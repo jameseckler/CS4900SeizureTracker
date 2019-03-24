5baca14a0a540c0a6d07a797718db426fe5985ca
James Eckler <jameseckler1994@yahoo.com>
2019-03-23 22:28:04 -0400

    v0.3.6 - Updates to tests, pet list function

--- SeizureTracker/Functions/getPetList.js
+++ SeizureTracker/Functions/getPetList.js
@@ -1,16 +1,43 @@
+import React from "react";
 import { FlatList } from "react-native";
-import { ListItem } from "react-native-elements";
-import { firebase } from "firebase";
+import { List, ListItem } from "react-native-elements";
+import * as firebase from "firebase";
 
 function getPetList (ownerID) {
 
+    
     var fb = firebase.firestore();
 
-    var users = fb.collection('users').where('userID', "==", ownerID).get();
+    var pets;
+    var i = 0;
+    for (id in ownerID) {
+        var users = fb.collection('users').where('userID', "==", id).get();
+
+        for (user in users) {
+            var j = 0;
+            while (j < user.pets.length) {
+                pets[i] = {
+                    firstName: user.firstName,
+                    lastName: user.lastName,
+                    petName: user.pets[j].doc                    
+                };
+                i++;
+                j++;
+            }
+        }
+    }
 
     return (
         <List>
             <FlatList
+                data= { pets }
+                renderItem = {({ pet }) => (
+                    <ListItem
+                        title = { user.petName }
+                        subtitle = {`${user.firstName} ${user.lastName}`}
+                        keyExtractor = { user }
+                    />
+                )}
             />
         </List>
     );
